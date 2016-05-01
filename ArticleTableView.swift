a564da1cb140b920f992ba949d0a0fbc71b9684d
dddisk <13da144@gmail.com>
2016-05-01 16:38:44 +0900

    rssリーダーフレーム完成

--- ArticleTableView.swift
+++ ArticleTableView.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ArticleTableView: UITableView,UITableViewDelegate, UITableViewDataSource {
     //imageMaskViewの背景色
-    let blue = UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+//    let blue = UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    var siteName: String!
+    var siteImageName: String!
+    var color: UIColor!
     //ソースコードでインスタンスを生成した場合
     override init(frame: CGRect, style: UITableViewStyle) {
         super.init(frame: frame, style: style)
@@ -18,25 +21,57 @@ class ArticleTableView: UITableView,UITableViewDelegate, UITableViewDataSource {
         self.delegate = self
         self.dataSource = self
         self.registerNib(UINib(nibName: "SiteTopTableViewCell", bundle: nil), forCellReuseIdentifier: "SiteTopTableViewCell")
+        self.registerNib(UINib(nibName: "ArticleTableViewCell", bundle: nil), forCellReuseIdentifier: "ArticleTableViewCell")
+
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
     //セル数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if section == 0 {
+            return 1
+        } else {
+            return 10
+        }
     }
-    //セル内容
+    //セルの内容
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("SiteTopTableViewCell", forIndexPath: indexPath) as! SiteTopTableViewCell
-        cell.siteImageView.image = UIImage(named: "wired_top_image")
-        cell.siteName.text = "WIRED"
-        cell.imageMaskView.backgroundColor = blue
-        return cell
-    }
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("SiteTopTableViewCell", forIndexPath: indexPath) as! SiteTopTableViewCell
+//            cell.siteImageView.image = UIImage(named: "wired_top_image")
+//            cell.siteName.text = "WIRED"
+//            cell.imageMaskView.backgroundColor = blue
+            cell.siteImageView.image = UIImage(named: self.siteImageName)
+            cell.siteName.text = self.siteName
+            cell.imageMaskView.backgroundColor = self.color
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("ArticleTableViewCell", forIndexPath: indexPath) as! ArticleTableViewCell
+            return cell
+       }
+       }
     //セルの高さ
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 200
+        if indexPath.section == 0 {
+            return 200
+        } else {
+            return 85
+        }
+        }
+    //urlの読み込み
+    func loadRSS(siteURL: String) {
+        if let url = NSURL(string: siteURL) {
+            let request = NSURLRequest(URL: url)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithRequest(request, completionHandler: {
+                (data, response, error) -> Void in
+                print(data)
+                })
+            task.resume()
+        }
     }
-
 }
--- news1kenkou/ArticleViewController.swift
+++ news1kenkou/ArticleViewController.swift
@@ -8,28 +8,136 @@
 
 import UIKit
 
-class ArticleViewController: UIViewController {
+class ArticleViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var sitesScrollView: UIScrollView!
-
+    //ボタンを管理するための配列を定義
+    var tabButtons:Array<UIButton> = []
+    
+    let wired = "WIRED"
+    let shiki = "100SHIKI"
+    let cinra = "CINRA.NET"
+    
+    let wiredImageName = "wired_top_image.png"
+    let shikiImageName = "100shiki_top_image.png"
+    let cinraImageName = "cinra_top_image.png"
+    
+    let blue = UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    let yellow = UIColor(red: 105.0 / 255, green: 207.0 / 255, blue: 153.0 / 255, alpha: 1.0)
+    let red = UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    let black = UIColor(red: 50.0 / 255, green: 56.0 / 255, blue: 60.0 / 255, alpha: 1.0)
+    //サイトURL
+    let wiredURL = "http://xn--7ckd4fsby326e.com/"
+    let shikiURL =  "http://www.100shiki.com/feed"
+    let cinraURL =   "http://www.cinra.net/feed"
+    //ボタンを生成
+    func setTabButton(x: CGFloat, text: String, color: UIColor, tag: Int){
+    var tabButton = UIButton()
+        tabButton.frame.size = CGSizeMake(36, 36)
+        tabButton.center = CGPointMake(x, 44)
+        tabButton.setTitle(text, forState: UIControlState.Normal)
+        tabButton.setTitleColor(color, forState: UIControlState.Selected)
+        tabButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        tabButton.titleLabel?.font = UIFont(name: "HirakakuProN-W6", size: 13)
+        tabButton.backgroundColor = black
+        tabButton.tag = tag
+        tabButton.addTarget(self, action: #selector(ArticleViewController.tapTabButton(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        tabButton.layer.cornerRadius = 18
+        tabButton.layer.borderColor = UIColor.whiteColor().CGColor
+        tabButton.layer.borderWidth = 1
+        tabButton.layer.masksToBounds = true
+        self.headerView.addSubview(tabButton)
+        tabButtons.append(tabButton)
+    }
+    //タップしたボタンの動き
+    func tapTabButton(sender: UIButton){
+        let pointX = self.view.frame.width * CGFloat(sender.tag - 1)
+        sitesScrollView.setContentOffset(CGPointMake(pointX, 0), animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        sitesScrollView.delegate = self
         //sitesScrollView
         self.sitesScrollView.contentSize = CGSizeMake(self.view.frame.width * 3, self.sitesScrollView.frame.height)
         self.sitesScrollView.pagingEnabled = true
-        //ArticleTableViewのインスタンスを生成
-        setArticleTableView(0)
-        setArticleTableView(self.view.frame.width)
-        setArticleTableView(self.view.frame.width*2)
+        //articleTableView
+        setArticleTableView(0, siteName: wired, siteImageName: wiredImageName, color: blue, siteURL: wiredURL)
+        setArticleTableView(self.view.frame.width, siteName: shiki, siteImageName: shikiImageName, color: red, siteURL: shikiURL)
+        setArticleTableView(self.view.frame.width*2, siteName: cinra, siteImageName: cinraImageName, color: yellow, siteURL: cinraURL)
+        //ボタンの見た目
+        setTabButton(self.view.center.x/2, text: "W", color: blue, tag: 1)
+        setTabButton(self.view.center.x, text: "100", color: red, tag: 2)
+        setTabButton(self.view.center.x*3/2, text: "C", color: yellow, tag: 3)
+    }
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        //下記のものでもできる
+//        //観覧中のページを取得(WIRED:page　== 0, 100SHKI: page == 1, CINRA: page == 2)
+//        let page = scrollView.contentOffset.x / scrollView.frame.width
+//        if page == 0 {
+//            setSelectedButton(tabButtons[0], selected: true)
+//            setSelectedButton(tabButtons[1], selected: false)
+//            setSelectedButton(tabButtons[2], selected: false)
+//        } else if page == 1 {
+//            setSelectedButton(tabButtons[0], selected: false)
+//            setSelectedButton(tabButtons[1], selected: true)
+//            setSelectedButton(tabButtons[2], selected: false)
+//        } else if page == 2 {
+//            setSelectedButton(tabButtons[0], selected: false)
+//            setSelectedButton(tabButtons[1], selected: false)
+//            setSelectedButton(tabButtons[2], selected: true)
+//        }
+//    }
+    //もしくは下記のものでもできる
+//    //観覧中のページを取得(WIRED:page　=＝ 0, 100SHKI: page == 1, CINRA: page == 2)
+//    let page = scrollView.contentOffset.x / scrollView.frame.width
+//    
+//    for num in 0..<3 {
+//    if page == CGFloat(num) {
+//    //閲覧中のページのみボタンを選択状態にする
+//    setSelectedButton(tabButtons[num], selected: true)
+//    } else {
+//    //選択されていないボタンは選択状態を解除する
+//    setSelectedButton(tabButtons[num], selected: false)
+//    }
+        //    } func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.frame.width
+        
+        for num in 0..<3 {
+            if page == CGFloat(num) {
+                setSelectedButton(tabButtons[num], selected: true)
+            } else {
+                setSelectedButton(tabButtons[num], selected: false)
+            }
+        }
     }
     
+    //ドラッグでのスクロールが終了時に呼ばれる
+    func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.frame.width
+        
+        for num in 0..<3 {
+            if page == CGFloat(num) {
+                setSelectedButton(tabButtons[num], selected: true)
+            } else {
+                setSelectedButton(tabButtons[num], selected: false)
+            }
+        }
+    }
+    
+    func setSelectedButton(button: UIButton, selected: Bool) {
+        button.selected = selected
+        button.layer.borderColor = button.titleLabel?.textColor.CGColor
+    }
     //ArticleTableViewを生成するためのメソッド
-    func setArticleTableView(x: CGFloat){
+    func setArticleTableView(x: CGFloat, siteName: String, siteImageName: String, color: UIColor, siteURL: String){
         let frame = CGRectMake(x, 0, self.view.frame.width, sitesScrollView.frame.height)
         let articleTableView = ArticleTableView(frame: frame, style: UITableViewStyle.Plain)
-        self.sitesScrollView.addSubview(articleTableView)
+        articleTableView.loadRSS(siteURL)
+        articleTableView.siteName = siteName
+        articleTableView.siteImageName = siteImageName
+        articleTableView.color = color
+        sitesScrollView.addSubview(articleTableView)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
